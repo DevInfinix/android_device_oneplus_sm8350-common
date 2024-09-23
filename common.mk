786cabe9419e084e2452897fab8002d49601b62e
DevInfinix <singhsamarjeet2007@gmail.com>
2024-09-24 03:17:33 +0530

    sm8350-common: Drop LiveDisplay

--- common.mk
+++ common.mk
@@ -281,11 +281,7 @@ PRODUCT_ENABLE_UFFD_GC := false
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@4.1.vendor		
-
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.1-service.oplus
+    android.hardware.keymaster@4.1.vendor
 
 # Media
 PRODUCT_PACKAGES += \
