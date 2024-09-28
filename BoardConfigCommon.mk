5e8062feede4495d307f32f10894acba2b9080f0
DevInfinix <singhsamarjeet2007@gmail.com>
2024-09-29 01:53:03 +0530

    sm8350--common: Drop `-fuse-ld=lld` from CFLAGS

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -113,7 +113,7 @@ TARGET_KERNEL_CONFIG := vendor/lahaina-qgki_defconfig
 TARGET_KERNEL_CLANG_VERSION := azure
 TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-$(TARGET_KERNEL_CLANG_VERSION)
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
-    HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
+    HOSTCFLAGS="-Wno-unused-command-line-argument"
 TARGET_KERNEL_NO_GCC := true
 KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-$(TARGET_KERNEL_CLANG_VERSION)/bin
 TARGET_KERNEL_CLANG_COMPILE := true
