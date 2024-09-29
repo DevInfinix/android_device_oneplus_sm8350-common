19c85f754cae80c3c7661642a04d45a7cf07b6d6
DevInfinix <singhsamarjeet2007@gmail.com>
2024-09-29 14:46:11 +0530

    sm8350-common: Fallback to default clang

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -110,14 +110,14 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_RAMDISK_USE_LZ4 := true
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8350
 TARGET_KERNEL_CONFIG := vendor/lahaina-qgki_defconfig
-TARGET_KERNEL_CLANG_VERSION := azure
-TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-$(TARGET_KERNEL_CLANG_VERSION)
+# TARGET_KERNEL_CLANG_VERSION := azure
+# TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-$(TARGET_KERNEL_CLANG_VERSION)
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     HOSTCFLAGS="-Wno-unused-command-line-argument"
 TARGET_KERNEL_NO_GCC := true
-KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-$(TARGET_KERNEL_CLANG_VERSION)/bin
-TARGET_KERNEL_CLANG_COMPILE := true
-KERNEL_CC := CC=clang
+# KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-$(TARGET_KERNEL_CLANG_VERSION)/bin
+# TARGET_KERNEL_CLANG_COMPILE := true
+# KERNEL_CC := CC=clang
 
 # Kernel modules
 BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(COMMON_PATH)/modules.blocklist
